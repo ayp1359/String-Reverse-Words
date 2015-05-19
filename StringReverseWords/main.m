08b9c28eb2ba5e99d65c70f6d7f4d4976d3764db
Ali Panah <ayp@fb.com>
2015-05-19 11:00:37 -0700

    first commit

--- StringReverseWords/main.m
+++ StringReverseWords/main.m
@@ -1,17 +1,21 @@
-//
-//  main.m
-//  StringReverseWords
-//
-//  Created by Ali Panah on 5/19/15.
-//  Copyright (c) 2015 Ali Panah. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[]){
   @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
+    
+    NSString *originalString = @"flying in a plane";
+    
+    NSArray *words = [originalString componentsSeparatedByString:@" "];
+    
+    NSMutableString *reversedString = [[NSMutableString alloc] initWithString:@""];
+    
+    for (int i=0;i<words.count;i++){
+      [reversedString appendFormat:@"%@ ",[words objectAtIndex:words.count-i-1]];
+    }
+    
+    NSLog(@"ORIGINAL:%@, REVERSED:%@",originalString,reversedString);
+    
   }
-    return 0;
+  
+  return 0;
 }
